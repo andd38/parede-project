9451e0161da9b86ee7f746c47c452931c0b73978
andre patriarca <andrepatriarca4@gmail.com>
2024-01-13 18:44:48 -0300

    armazenando valores no LocalStorage

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
+window.addEventListener('load',function(e){
+    e.preventDefault();
+    ValoresArmazenados;
+});
+
 const btnCalc = document.querySelector("#botao")
 btnCalc.addEventListener('click',(e)=>{
     e.preventDefault();
     calcular();
     console.log('ola');
     
-})
+});
 
 const btnLimpar = document.querySelector("#limpar")
 btnLimpar.addEventListener('click',(e) => {
     e.preventDefault();
-    Limpar()
-})
+    Limpar();
+});
 
 function calcular(){
     const largura=  Number(document.querySelector('#width').value);
     const altura =  Number(document.querySelector('#height').value);
     let result = largura*altura*0.37;
-    document.querySelector('#resultado').innerHTML = result.toFixed(2);
+
+    if(!isNaN(largura)  && !isNaN(altura)){
+
+        document.querySelector('#resultado').innerHTML = result.toFixed(2);
+        localStorage.setItem('resultado',result.toFixed(2));
+
+    } else{
+
+        alert('insira valores validos');
+    }
     
+}
+
+function ValoresArmazenados(){
+    
+    const alturaArmazenada = localStorage.getItem('altura');
+    const larguraArmazenada = localStorage.getItem('largura');
+    const resultadoArmazenado = localStorage.getItem('resultado');
+
+    document.querySelector('#height').value=alturaArmazenada;
+    document.querySelector('#width').value= larguraArmazenada ;
+    document.querySelector('#resultado').value=resultadoArmazenado ;
+
+   
 }
 
 function Limpar(){
@@ -25,7 +52,7 @@ function Limpar(){
     let alturaLimpa =  document.querySelector('#height').value='';
 
 
-    let resultado = "Aqui aparecerá  o resulatado do seu calculo, indicando quantos baldes de tinta serão necessarios para pintar a parede";
+    let resultado = "Aqui aparecerá  o resultado do seu calculo, indicando quantos baldes de tinta serão necessarios para pintar a parede";
     document.querySelector('#resultado').innerHTML = resultado;
     
 
