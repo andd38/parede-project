b60444fe2926344a07e3fcfcab446ef48a4e4c71
andre patriarca <andrepatriarca4@gmail.com>
2024-01-14 15:12:45 -0300

    errros 2

--- script.js
+++ script.js
@@ -3,35 +3,35 @@ window.addEventListener('load',ValoresArmazenados);
 const btnCalc = document.querySelector("#botao")
 btnCalc.addEventListener('click',(e)=>{
     e.preventDefault();
+    
     calcular();
-   
+    
     
 });
+
 
 const btnLimpar = document.querySelector("#limpar")
 btnLimpar.addEventListener('click',(e) => {
     e.preventDefault();
     Limpar();
 });
-
 function calcular(){
     const largura=  Number(document.querySelector('#width').value);
     const altura =  Number(document.querySelector('#height').value);
+    
     let result = largura*altura*0.37;
 
-    if(!isNaN(largura)  && !isNaN(altura)){
-
-        document.querySelector('#resultado').innerHTML = result.toFixed(2);
+    if(isNaN(result)){
+        document.querySelector('#resultado').innerHTML = ` Aqui aparecerá  o resultado do seu calculo, indicando quantos baldes de tinta serão necessários para pintar a parede`; 
+    }
+    else{
+        document.querySelector('#resultado').innerHTML = ` a quantidade de baldes necessarias para pintar a parede é ${result.toFixed(2)}`;
         localStorage.setItem('width',largura);
         localStorage.setItem('height',altura);
         localStorage.setItem('resultado',result.toFixed(2));
-       
 
-
-    } else{
-
-        alert('insira valores validos');
     }
+       
     
 }
 
@@ -43,7 +43,10 @@ function ValoresArmazenados(){
 
     document.querySelector('#height').value=alturaArmazenada;
     document.querySelector('#width').value= larguraArmazenada ;
-    document.querySelector('#resultado').innerHTML=resultadoArmazenado ;
+    
+    document.querySelector('#resultado').innerHTML = `A quantidade de baldes necessários para pintar a parede é ${resultadoArmazenado}`;
+
+    
 
    
    
@@ -53,11 +56,11 @@ function Limpar(){
     let larguraLimpa=  document.querySelector('#width').value = '';
     let alturaLimpa =  document.querySelector('#height').value='';
 
-
+    
     let resultado = "Aqui aparecerá  o resultado do seu calculo, indicando quantos baldes de tinta serão necessarios para pintar a parede";
     document.querySelector('#resultado').innerHTML = resultado;
-
     localStorage.clear();
+    
     
 
 }
